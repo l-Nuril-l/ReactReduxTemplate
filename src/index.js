7816d286c78658ff2b4468bed0240ebc1baeb07c
Валера Коршун <korshunval2033@gmail.com>
2022-02-07 17:43:17 +0200

    Router

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/store'
 
 
 ReactDOM.render(
-  
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  
+  <BrowserRouter>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<App />}/>
+      </Routes>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
-  
 );
 
